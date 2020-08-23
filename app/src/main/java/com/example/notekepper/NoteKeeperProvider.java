1a3a6b389e186810cd62b93dadd7abdbbd9a311f
Nam Vu <vungocnam0409@gmail.com>
2020-08-23 12:16:22 +0700

    Add data for query each note of list notes

--- app/src/main/java/com/example/notekepper/NoteKeeperProvider.java
+++ app/src/main/java/com/example/notekepper/NoteKeeperProvider.java
@@ -1,6 +1,7 @@
 package com.example.notekepper;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
@@ -26,13 +27,14 @@ public class NoteKeeperProvider extends ContentProvider {
 
     public static final int COURSES = 0;
     public static final int NOTES = 1;
-
     public static final int NOTES_EXPANDED = 2;
+    public static final int NOTES_ROW = 3;
 
     static {
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Courses.PATH, COURSES);
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH, NOTES);
         sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH_EXPANDED, NOTES_EXPANDED);
+        sUriMatcher.addURI(NoteKeeperProviderContract.AUTHORITY, Notes.PATH + "/#", NOTES_ROW);
     }
 
     public NoteKeeperProvider() {
@@ -53,8 +55,28 @@ public class NoteKeeperProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        SQLiteDatabase db = mDbOpenHelper.getWritableDatabase();
+        Uri rowUri = null;
+        long rowId = -1;
+        int uriCode = sUriMatcher.match(uri);
+        switch (uriCode) {
+            case NOTES: {
+                rowId = db.insert(NoteInfoEntry.TABLE_NAME, null, values);
+//                "content://com.example.notekepper.provider/notes/1"
+                rowUri = ContentUris.withAppendedId(Notes.CONTENT_URI, rowId);
+                break;
+            }
+            case COURSES: {
+                rowId = db.insert(CourseInfoEntry.TABLE_NAME, null, values);
+                rowUri = ContentUris.withAppendedId(Notes.CONTENT_URI, rowId);
+                break;
+            }
+            case NOTES_EXPANDED: {
+                break;
+            }
+            default:
+        }
+        return rowUri;
     }
 
     @Override
@@ -70,7 +92,7 @@ public class NoteKeeperProvider extends ContentProvider {
         SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
 
         int uriMatch = sUriMatcher.match(uri);
-        switch(uriMatch) {
+        switch (uriMatch) {
             case COURSES:
                 cursor = db.query(CourseInfoEntry.TABLE_NAME, projection, selection, selectionArgs,
                         null, null, sortOrder);
@@ -82,6 +104,12 @@ public class NoteKeeperProvider extends ContentProvider {
             case NOTES_EXPANDED:
                 cursor = notesExpandedQuery(db, projection, selection, selectionArgs, sortOrder);
                 break;
+            case NOTES_ROW:
+                long rowId = ContentUris.parseId(uri);
+                String rowSelection = NoteInfoEntry._ID + " = ? ";
+                String[] rowSelectionArgs = new String[]{Long.toString(rowId)};
+                cursor = db.query(NoteInfoEntry.TABLE_NAME, projection, rowSelection, rowSelectionArgs,
+                        null, null, null);
         }
         return cursor;
     }
@@ -90,7 +118,7 @@ public class NoteKeeperProvider extends ContentProvider {
                                       String[] selectionArgs, String sortOrder) {
 
         String[] columns = new String[projection.length];
-        for(int idx=0; idx < projection.length; idx++) {
+        for (int idx = 0; idx < projection.length; idx++) {
             columns[idx] = projection[idx].equals(BaseColumns._ID) ||
                     projection[idx].equals(CoursesIdColumns.COLUMN_COURSE_ID) ?
                     NoteInfoEntry.getQName(projection[idx]) : projection[idx];
